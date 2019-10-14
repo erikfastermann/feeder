ce8e2edc460453e2461df046cab801c24565fb18
Erik Fastermann <erik@fastermann.de>
2019-10-14 09:28:35 +0200

    handler-overview: Added subpages

--- handler/overview.go
+++ handler/overview.go
@@ -2,11 +2,27 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/erikfastermann/httpwrap"
 )
 
 func (h *Handler) overview(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	items, err := h.DB.Newest(ctx, 0, 30)
+	pageStr := r.FormValue("page")
+	page := uint64(0)
+	if pageStr != "" {
+		var err error
+		page, err = strconv.ParseUint(pageStr, 10, 0)
+		if err != nil {
+			return httpwrap.Error{
+				StatusCode: http.StatusBadRequest,
+				Err:        fmt.Errorf("overview: ivalid page %s", strconv.Quote(pageStr)),
+			}
+		}
+	}
+	items, err := h.DB.Newest(ctx, uint(page*30), 30)
 	if err != nil {
 		return err
 	}
