5efaee9de81647a4ab455edc7b36f14ecc646253
Erik Fastermann <erik@fastermann.de>
2019-12-02 13:15:48 +0100

    overview: Added back/next button

--- handler/overview.go
+++ handler/overview.go
@@ -6,25 +6,47 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/erikfastermann/feeder/db"
 	"github.com/erikfastermann/httpwrap"
 )
 
 func (h *Handler) overview(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	pageStr := r.FormValue("page")
-	page := uint64(0)
-	if pageStr != "" {
-		var err error
-		page, err = strconv.ParseUint(pageStr, 10, 0)
-		if err != nil {
+	page := uint(0)
+	if pageStr := r.FormValue("page"); pageStr != "" {
+		page64, err := strconv.ParseUint(pageStr, 10, 0)
+		page = uint(page64)
+		const uintMax = ^uint(0)
+		if err != nil || page > uintMax/30 {
 			return httpwrap.Error{
 				StatusCode: http.StatusBadRequest,
 				Err:        fmt.Errorf("overview: ivalid page %s", strconv.Quote(pageStr)),
 			}
 		}
 	}
-	items, err := h.DB.Newest(ctx, uint(page*30), 30)
+
+	count, err := h.DB.ItemCount(ctx)
 	if err != nil {
 		return err
 	}
-	return h.tmplts.ExecuteTemplate(w, "overview.html", items)
+	const itemsPerPage = 30
+	offset := page * itemsPerPage
+	items, err := h.DB.Newest(ctx, offset, itemsPerPage)
+	if err != nil {
+		return err
+	}
+
+	next := int(page) + 1
+	if offset+itemsPerPage > uint(count) {
+		next = -1
+	}
+
+	return h.tmplts.ExecuteTemplate(w, "overview.html", struct {
+		Prev  int
+		Next  int
+		Items []db.ItemWithHost
+	}{
+		Prev:  int(page) - 1,
+		Next:  next,
+		Items: items,
+	})
 }
